db0d2352c9270a8ff8e81a6a628cf9352fdad707
Karol <karol3883@gmail.com>
2022-01-13 00:02:03 +0100

    main - import bootstrap vue, register project

---
+++ assets/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <h1> Welcome from component! </h1>
+    <button @click="counter++">
+      Click me {{ counter }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+      counter: 0,
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ assets/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+    <h1> Welcome from component! </h1>
+    <button @click="counter++">
+      Click me {{ counter }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+      counter: 0,
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
